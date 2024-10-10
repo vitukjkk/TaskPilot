54fd47d1fd21a3f9132d750a4f97ae68bad60225
Vitor De Oliveira Colombelli <06207288173@DCCBRA.IFB.LOCAL>
2024-10-10 15:19:31 -0300

    AAAAAAAAA

--- script/index.js
+++ script/index.js
@@ -1,19 +1,10 @@
+alert('ola mundo')
+
+import * as mainFunctions from './main.js'
+
 window.onload = () => {
-    if(window.matchMedia('(prefers-color-scheme: light)').matches) {
-        defineVariables()
-        imgMainLogo.style.filter = 'grayscale(100%) brightness(0%)'
-        imgMainUser.style.filter = 'grayscale(100%) brightness(0%)'
-        imgMainConfig.style.filter = 'grayscale(100%) brightness(0%)'
-    } else {
-        
-    }
+    mainFunctions.changeTheme()       
 }
 
 // FUNCTIONS
 
-function defineVariables() {
-    // VARIÁVEIS HEADER
-    const imgMainLogo = document.querySelector('#imgMainLogo')
-    const imgMainUser = document.querySelector('#imgMainUser')
-    const imgMainConfig = document.querySelector('#imgMainConfig')
-}
--- script/main.js
+++ script/main.js
@@ -2,16 +2,30 @@
 // ELEMENTOS
 // ===========================
 
-// IMAGES
+// MANAGE
 
+alert('ola mundo')
 
-function getBrowserTheme() {
+function defineVariables() {
+    // VARIÁVEIS HEADER
+    const imgMainLogo = document.querySelector('#imgMainLogo')
+    const imgMainUser = document.querySelector('#imgMainUser')
+    const imgMainConfig = document.querySelector('#imgMainConfig')
 }
 
-function setColorTheme() {
 
+function changeTheme() {
+    alert('ola mundo')
+    if(window.matchMedia('(prefers-color-scheme: light)').matches) {
+        defineVariables()
+        imgMainLogo.style.filter = 'grayscale(100%) brightness(0%)'
+        imgMainUser.style.filter = 'grayscale(100%) brightness(0%)'
+        imgMainConfig.style.filter = 'grayscale(100%) brightness(0%)'
+    } else {
+        
+    }
 }
 
 export {
-
+    changeTheme, defineVariables
 }
