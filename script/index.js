2c8a8f2aac2a1cca6758a2996e9cd127d5a845cd
Vitor De Oliveira Colombelli <06207288173@DCCBRA.IFB.LOCAL>
2024-10-10 14:39:45 -0300

    estilizando header

--- script/index.js
+++ script/index.js
@@ -2,6 +2,8 @@ window.onload = () => {
     if(window.matchMedia('(prefers-color-scheme: light)').matches) {
         defineVariables()
         imgMainLogo.style.filter = 'grayscale(100%) brightness(0%)'
+        imgMainUser.style.filter = 'grayscale(100%) brightness(0%)'
+        imgMainConfig.style.filter = 'grayscale(100%) brightness(0%)'
     } else {
         
     }
@@ -10,5 +12,8 @@ window.onload = () => {
 // FUNCTIONS
 
 function defineVariables() {
+    // VARIÁVEIS HEADER
     const imgMainLogo = document.querySelector('#imgMainLogo')
+    const imgMainUser = document.querySelector('#imgMainUser')
+    const imgMainConfig = document.querySelector('#imgMainConfig')
 }
