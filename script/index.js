6bed7ddd1f60babb0aecdd3cc7d2baf86e1cc7e5
Vitor De Oliveira Colombelli <06207288173@dccbra.ifb.local>
2024-10-10 14:12:35 -0300

    retomando

--- script/index.js
+++ script/index.js
@@ -1,10 +1,14 @@
-const imgMainLogo = document.querySelector('#imgMainLogo')
-
 window.onload = () => {
-
     if(window.matchMedia('(prefers-color-scheme: light)').matches) {
-        document.querySelector('#imgMainLogo').style.filter = 'grayscale(100%) brightness(0%)'
+        defineVariables()
+        imgMainLogo.style.filter = 'grayscale(100%) brightness(0%)'
     } else {
         
     }
+}
+
+// FUNCTIONS
+
+function defineVariables() {
+    const imgMainLogo = document.querySelector('#imgMainLogo')
 }
