6785d4cea6ccab363a1b03ec174648ce88e854c6
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-05 14:51:17 -0300

    feat: fazendo o header

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.module.css";
+
+export default function Footer() {
+    return(
+        <footer>
+            <p>
+                2024 - todos os direitos reservados - por <a href="https://github.com/vitukjkk" target="_blank">vitukjkkk</a>
+            </p>
+        </footer>
+    )
+}
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Header.module.css";
+
+export default function Header() {
+    return(
+        <header>
+            <p>Header</p>
+        </header>
+    )
+}
